5420bb83a1ed35c3077b826a4a24ad092f77282f
Angus-Warman <anguswarman@gmail.com>
2026-09-15 16:08:04 +0800

    Include test popup in demo

--- demo/main.go
+++ demo/main.go
@@ -19,5 +19,10 @@ func main() {
 			fmt.Fprintf(w, "pong %v", time.Now())
 		})
 
+	app.HandleFunc("/api/popup", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		go app.Popup("Hello World", "Test Popup")
+	})
+
 	app.Launch()
 }
