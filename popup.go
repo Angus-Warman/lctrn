d503d9f95b53f258fd386b99f86fd0332e761565
Angus-Warman <anguswarman@gmail.com>
2026-09-16 13:04:33 +0800

    Update popup.go

--- popup.go
+++ popup.go
@@ -6,49 +6,114 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
+	"strings"
 	"unicode/utf16"
 )
 
 type popupMethod struct {
-	name     string
-	callback func(title, msg string) error
+	opsys string
+	tool  string
+	fn    func(title, msg string) error
 }
 
 var popupMethods = []popupMethod{
 	{
-		name: "osascript",
-		callback: func(title, msg string) error {
-			return exec.Command("osascript", "-e",
-				fmt.Sprintf(`display dialog "%v" with title "%v" buttons {"OK"} with icon stop`, msg, title),
-			).Run()
+		opsys: "darwin",
+		tool:  "osascript",
+		fn: func(title, msg string) error {
+			title = strings.ReplaceAll(title, `"`, `\"`)
+			msg = strings.ReplaceAll(msg, `"`, `\"`)
+			command := fmt.Sprintf(`display dialog "%v" with title "%v" buttons {"OK"} with icon stop`, msg, title)
+			return exec.Command("osascript", "-e", command).Run()
 		},
 	},
 	{
-		name: "powershell",
-		callback: func(title, msg string) error {
+		opsys: "windows",
+		tool:  "powershell",
+		fn: func(title, msg string) error {
+			title = strings.ReplaceAll(title, "'", "''")
+			msg = strings.ReplaceAll(msg, "'", "''")
 			command := fmt.Sprintf("Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('%v','%v',0,16)", msg, title)
 			command = encodeUTF16LE(command)
 			return exec.Command("powershell", "-NoProfile", "-EncodedCommand", command).Run()
 		},
 	},
 	{
-		name: "zenity",
-		callback: func(title, msg string) error {
+		opsys: "",
+		tool:  "zenity",
+		fn: func(title, msg string) error {
 			return exec.Command("zenity", "--error", "--title="+title, "--text="+msg).Run()
 		},
 	},
 	{
-		name: "kdialog",
-		callback: func(title, msg string) error {
+		opsys: "",
+		tool:  "kdialog",
+		fn: func(title, msg string) error {
 			return exec.Command("kdialog", "--error", msg, "--title", title).Run()
 		},
 	},
 	{
-		name: "xmessage",
-		callback: func(title, msg string) error {
-			return exec.Command("xmessage", "-center", msg).Run()
+		opsys: "",
+		tool:  "xmessage",
+		fn: func(title, msg string) error {
+			msg = fmt.Sprintf("%v\n\n%v", title, msg)
+			return exec.Command("xmessage", "-center", "-geometry", "400x200", msg).Run()
 		},
 	},
+
+	// Text file fallbacks
+	{
+		opsys: "darwin",
+		tool:  "open",
+		fn: func(title, msg string) error {
+			f, err := createTempFile(title, msg)
+			if err != nil {
+				return err
+			}
+			return exec.Command("open", f).Run()
+		},
+	},
+	{
+		opsys: "",
+		tool:  "xdg-open",
+		fn: func(title, msg string) error {
+			f, err := createTempFile(title, msg)
+			if err != nil {
+				return err
+			}
+			return exec.Command("xdg-open", f).Run()
+		},
+	},
+	{
+		opsys: "windows",
+		tool:  "cmd",
+		fn: func(title, msg string) error {
+			f, err := createTempFile(title, msg)
+			if err != nil {
+				return err
+			}
+			return exec.Command("cmd", "/c", "start", f).Run()
+		},
+	},
+}
+
+func createTempFile(title, msg string) (string, error) {
+	f, err := os.CreateTemp("", "popup__*.txt")
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Fprintf(f, "%v\n\n%v", title, msg)
+
+	if err := f.Sync(); err != nil {
+		return "", err
+	}
+	if err := f.Close(); err != nil {
+		return "", err
+	}
+
+	return f.Name(), err
 }
 
 func encodeUTF16LE(s string) string {
@@ -60,44 +125,27 @@ func encodeUTF16LE(s string) string {
 	return base64.StdEncoding.EncodeToString(data)
 }
 
-func popupUsingTxtFile(title, msg string) error {
-	f, err := os.CreateTemp("", fmt.Sprintf("%v__*.txt", title))
-	if err != nil {
-		return err
-	}
-
-	if _, err := f.WriteString(msg); err != nil {
-		return err
-	}
-	if err := f.Sync(); err != nil {
-		return err
-	}
-	if err := f.Close(); err != nil {
-		return err
-	}
-
-	return exec.Command("open", f.Name()).Run()
-}
-
-func (app *App) configurePopup() {
-	if app.popupFunc != nil {
-		return
-	}
-
+func (app *App) configurePopup() bool {
 	for _, method := range popupMethods {
-		_, err := exec.LookPath(method.name)
+		if method.opsys != "" && method.opsys != runtime.GOOS {
+			continue
+		}
+
+		_, err := exec.LookPath(method.tool)
 		if err == nil {
-			app.popupFunc = method.callback
-			return
+			app.popupFunc = method.fn
+			return true
 		}
 	}
 
-	app.popupFunc = popupUsingTxtFile
+	return false
 }
 
 func (app *App) Popup(title, msg string) {
 	if app.popupFunc == nil {
-		app.configurePopup()
+		if !app.configurePopup() {
+			log.Println("no available popup method")
+		}
 	}
 
 	err := app.popupFunc(title, msg)
